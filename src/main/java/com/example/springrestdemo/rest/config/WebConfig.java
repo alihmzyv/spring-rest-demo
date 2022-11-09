29bde49c0e24b818b32270de6bd476bbce059500
alihmzyv <alihmzyv@gmail.com>
2022-11-09 18:45:55 +0400

    added view controller

--- src/main/java/com/example/springrestdemo/rest/config/WebConfig.java
+++ src/main/java/com/example/springrestdemo/rest/config/WebConfig.java
@@ -1,6 +1,7 @@
-package com.example.springrestdemo.config;
+package com.example.springrestdemo.rest.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/api").setViewName("home");
+        registry.addViewController("/").setViewName("home");
     }
 }
